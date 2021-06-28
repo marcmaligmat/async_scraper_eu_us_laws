f8469cc8de5c71c34351e81bbd3bdfbe316786a3
marc <marcmaligmat@gmail.com>
2021-06-28 19:56:32 +0800

    updates on absl flags and change the code to Class

--- database_ipi_ch/main.py
+++ database_ipi_ch/main.py
@@ -1,142 +1,171 @@
 import base64
 import json
-from time import sleep
+
+from absl import app, flags
+from math import ceil
+
+from rich import pretty
+from rich.live import Live
+from rich.console import Console
+
+from post_data import post_data_from_oldest
 
 import re
 import requests
-from rich import inspect, pretty
-from rich.progress import Progress
-from rich.live import Live
+import os
 
-from post_data import post_data_from_oldest
+console = Console()
 pretty.install()
 
-# Results = PAGE_SIZE * LOOP
-PAGE_SIZE = 64 #use 64 as default
-LOOP = 1000
-OUTPUT_FILE = 'output.jsonl'
+
 LAST_CURSOR_FILE = 'last_cursor.jsonl'
 
-
-def generate_string(num1,last_expected_results):
-    return f"Scraped {num1} / {(PAGE_SIZE * LOOP) + last_expected_results} "
-
-
-def run(session):
-    print('Initializing')
-    cursor = {
-        'last_cursor':'*',
-        'page_number': 0,
-        'expected_results': 0
-    }
-    num = 0
-    original_expected_results = 0
-    
-
-    try:
-        cursor = get_last_cursor_object()
-        num = cursor['expected_results']
-        original_expected_results = cursor['expected_results']
-    except:
-        save_last_cursor(cursor)
-
-    with Live() as live:
-        with open(OUTPUT_FILE, 'a+') as f:
-        
-            for _ in range(LOOP):
-                response = send_post(session,cursor['last_cursor'])
-                api_results = response.json()['results']
-
-                results = '';
-                for result in api_results:
-                    if "bild_screen_hash__type_string_mv" in result.keys():
-                        result['base64_logo'] = [base64_decoded_utf8(result["bild_screen_hash__type_string_mv"][0])]
-                    results += json.dumps(result) + '\n'
-                    
-
-                    num += 1
-                    live.update(generate_string(num,original_expected_results))
-                f.write(results)
-
-                cursor = {
-                    'last_cursor':get_next_cursor(response),
-                    'page_number': cursor['page_number'] + 1,
-                    'expected_results': cursor['expected_results'] + PAGE_SIZE
-                }
-                save_last_cursor(cursor)
-
-        f.close()
+FLAGS = flags.FLAGS
+flags.DEFINE_boolean('debug', False, 'Produces debugging output.')
+flags.DEFINE_enum('page_size', '64', [
+                  '8', '16', '32', '64'], 'Number of results for each page.', short_name='ps')
+flags.DEFINE_string('output_file', 'output.jsonl',
+                    'Name of the file.', short_name='of')
+flags.DEFINE_string('output_dir', 'output.jsonl',
+                    'Directory of the output file', short_name='od')
 
 
 
 
-def send_post(session,cursor):
-    headers = {
-        'content-type': 'application/transit+json',
-        'X-IPI-VERSION': '2.1.3',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
-    }
-    
-    response = session.post(
-        f'https://database.ipi.ch/database/resources/query/fetch?ps={PAGE_SIZE}', 
-        headers=headers, 
-        json=post_data_from_oldest(PAGE_SIZE,cursor))
+class Database_ipi_ch():
+    def __init__(self):
+        console.print("Initializing!", style="green on black")
+        app.run(self.main)
 
-    return response
+    def main(self, argv):
+        self.session = requests.Session()
+        if FLAGS.debug:
+            print('non-flag arguments:', argv)
+        self.page_size = int(FLAGS.page_size)
 
 
-def get_next_cursor(response):
-    cursor_result = response.json()['metadataAsTransit']
-    cursor = cursor_result.replace('\\', '')
-    return json.loads(cursor)['~#resultmeta']['~:cursor']['~#opt']
+        if FLAGS.output_dir != 'output.jsonl':
+            self.output_file = FLAGS.output_dir
+            os.makedirs(os.path.dirname(self.output_file), exist_ok=True)
+        else:
+            if '.json' not in FLAGS.output_file:
+                self.output_file = f"{FLAGS.output_file}.jsonl"
+            else:
+                self.output_file = FLAGS.output_file
 
+        cursor = {
+            'last_cursor': '*',
+            'page_number': 0,
+            'expected_results': 0
+        }
+        num = 0
 
-def base64_decoded_utf8(image_slug):
-    url = f"https://database.ipi.ch/database/resources/image/{image_slug}"
-    base64_encoded_data = base64.b64encode(requests.get(url).content)
-    return base64_encoded_data.decode('utf-8')
+        try:
+            cursor = self.get_last_cursor_object()
+            num = cursor['expected_results']
+            original_expected_results = cursor['expected_results']
+        except:
+            self.save_last_cursor(cursor)
 
+        self.send_post(cursor['last_cursor'])
+        self.set_total_items()
+        self.loop = ceil(
+            (self.total_items-cursor['expected_results'])/self.page_size)
 
-def transform_to_image(base64_img):
-    base64_img_bytes = base64_img.encode('UTF-8') 
-    with open('decoded_image.png', 'wb') as file_to_save:
-        decoded_image_data = base64.decodebytes(base64_img_bytes)
-        file_to_save.write(decoded_image_data)
+        with Live() as live:
+            with open(self.output_file, 'a+') as f:
 
+                for _ in range(self.loop):
+                    self.send_post(cursor['last_cursor'])
+                    api_results = self.response.json()['results']
 
-def save_last_cursor(last_cursor):
-    handle_jsonl('w+',last_cursor, LAST_CURSOR_FILE)
+                    results = ''
+                    for result in api_results:
+                        if "bild_screen_hash__type_string_mv" in result.keys():
+                            result['base64_logo'] = [self.base64_decoded_utf8(
+                                result["bild_screen_hash__type_string_mv"][0])]
+                        results += json.dumps(result) + '\n'
 
+                        num += 1
+                        live.update(self.generate_string(num))
+                    f.write(results)
 
-def open_last_cursor():
-    f = open("last_cursor.txt", "r")
-    words = f.read()
-    m = re.search(r'last_cursor:(.+)',words)
-    return m.group(1)
+                    cursor = {
+                        'last_cursor': self.get_next_cursor(),
+                        'page_number': cursor['page_number'] + 1,
+                        'expected_results': cursor['expected_results'] + self.page_size
+                    }
+                    self.save_last_cursor(cursor)
 
+            f.close()
 
-def handle_jsonl(mode, json_data, output_file):
-    """ Example mode: 'w+','a+' """
-    with open(output_file, mode) as f:
-        f.write(json.dumps(json_data) + '\n')
-        # json.dump(json_data, outfile,ensure_ascii=False, indent=4)
-        # outfile.write('\n')
-        
+    def generate_string(self, num1):
+        return f"Scraped {num1} / {self.total_items} "
 
+    def send_post(self, cursor):
+        headers = {
+            'content-type': 'application/transit+json',
+            'X-IPI-VERSION': '2.1.3',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
+        }
 
-def get_last_cursor_object():
-    """Returns a dictionary from last_cursor.jsonl file"""
-    with open('last_cursor.jsonl', 'r') as json_file:
-        json_list = list(json_file)
-        last_cursor_object = ''
+        self.response = self.session.post(
+            f'https://database.ipi.ch/database/resources/query/fetch?ps={self.page_size}',
+            headers=headers,
+            json=post_data_from_oldest(self.page_size, cursor))
 
-        for _list in json_list:
-            _dict = _list.splitlines()
-            last_cursor_object += _dict[0]
+    def get_next_cursor(self):
+        cursor_result = self.response.json()['metadataAsTransit']
+        cursor = cursor_result.replace('\\', '')
+        return json.loads(cursor)['~#resultmeta']['~:cursor']['~#opt']
 
-        return json.loads(last_cursor_object)
+    def get_total_items(self):
+        cursor_result = self.response.json()['metadataAsTransit']
+        cursor = cursor_result.replace('\\', '')
+        return json.loads(cursor)['~#resultmeta']['~:total-items']
+
+    def set_total_items(self):
+        self.total_items = self.get_total_items()
+
+    def base64_decoded_utf8(self, image_slug):
+        url = f"https://database.ipi.ch/database/resources/image/{image_slug}"
+        base64_encoded_data = base64.b64encode(requests.get(url).content)
+        return base64_encoded_data.decode('utf-8')
+
+    def transform_to_image(self, base64_img):
+        base64_img_bytes = base64_img.encode('UTF-8')
+        with open('decoded_image.png', 'wb') as file_to_save:
+            decoded_image_data = base64.decodebytes(base64_img_bytes)
+            file_to_save.write(decoded_image_data)
+
+    def save_last_cursor(self, last_cursor):
+        self.handle_jsonl('w+', last_cursor, LAST_CURSOR_FILE)
+
+    def open_last_cursor(self):
+        f = open("last_cursor.txt", "r")
+        words = f.read()
+        m = re.search(r'last_cursor:(.+)', words)
+        return m.group(1)
+
+    def handle_jsonl(self, mode, json_data, output_file):
+        """ Example mode: 'w+','a+' """
+        with open(output_file, mode) as f:
+            f.write(json.dumps(json_data) + '\n')
+            # json.dump(json_data, outfile,ensure_ascii=False, indent=4)
+            # outfile.write('\n')
+
+    def get_last_cursor_object(self):
+        """Returns a dictionary from last_cursor.jsonl file"""
+        with open('last_cursor.jsonl', 'r') as json_file:
+            json_list = list(json_file)
+            last_cursor_object = ''
+
+            for _list in json_list:
+                _dict = _list.splitlines()
+                last_cursor_object += _dict[0]
+
+            return json.loads(last_cursor_object)
+
 
 if __name__ == '__main__':
-    session = requests.Session()
-    run(session)
-
+    Database_ipi_ch()

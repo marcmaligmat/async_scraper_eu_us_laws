2d1867adb6f8447a52e0c99ba8ee7fe6dc616581
marc <marcmaligmat@gmail.com>
2021-08-05 23:44:06 +0800

    added pydantic and a settings module

--- database_ipi_ch/main.py
+++ database_ipi_ch/main.py
@@ -1,7 +1,11 @@
 import base64
 import json
 
-from absl import app, flags
+import sys
+
+sys.path.append("..")
+from settings import Settings
+
 from math import ceil
 
 from rich import pretty
@@ -18,62 +22,56 @@ console = Console()
 pretty.install()
 
 
-
-FLAGS = flags.FLAGS
-flags.DEFINE_boolean('debug', False, 'Produces debugging output.')
-flags.DEFINE_integer('page_size', 64, 'Number of results for each page.')
-flags.DEFINE_string('output_file', './output.jsonl', 'Name of the file.')
-flags.DEFINE_string('last_cursor_file', 'last_cursor.json', 'Last cursor file')
-
-
-class Database_ipi_ch():
+class Database_ipi_ch:
     def scrape(self):
-        self.session = requests.Session()
-        self.page_size = FLAGS.page_size
+        self.settings = Settings()
 
-        self.output_file = FLAGS.output_file
+        self.session = requests.Session()
+
+        self.output_file = self.settings.output_file
         os.makedirs(os.path.dirname(self.output_file), exist_ok=True)
 
-        cursor = {
-            'last_cursor': '*',
-            'page_number': 0,
-            'expected_results': 0
-        }
+        cursor = {"last_cursor": "*", "page_number": 0, "expected_results": 0}
         num = 0
 
         try:
             cursor = self.get_last_cursor_object()
-            num = cursor['expected_results']
+            num = cursor["expected_results"]
         except:
             self.save_last_cursor(cursor)
 
-        self.send_post(cursor['last_cursor'])
+        self.send_post(cursor["last_cursor"])
         self.set_total_items()
         self.loop = ceil(
-            (self.total_items-cursor['expected_results'])/self.page_size)
+            (self.total_items - cursor["expected_results"]) / self.settings.page_size
+        )
 
         with Live() as live:
-            with open(self.output_file, 'a+') as f:
+            with open(self.output_file, "a+") as f:
 
                 for _ in range(self.loop):
-                    self.send_post(cursor['last_cursor'])
-                    api_results = self.response.json()['results']
+                    self.send_post(cursor["last_cursor"])
+                    api_results = self.response.json()["results"]
 
-                    results = ''
+                    results = ""
                     for result in api_results:
                         if "bild_screen_hash__type_string_mv" in result.keys():
-                            result['base64_logo'] = [self.base64_decoded_utf8(
-                                result["bild_screen_hash__type_string_mv"][0])]
-                        results += json.dumps(result) + '\n'
+                            result["base64_logo"] = [
+                                self.base64_decoded_utf8(
+                                    result["bild_screen_hash__type_string_mv"][0]
+                                )
+                            ]
+                        results += json.dumps(result) + "\n"
 
                         num += 1
                         live.update(self.generate_string(num))
                     f.write(results)
 
                     cursor = {
-                        'last_cursor': self.get_next_cursor(),
-                        'page_number': cursor['page_number'] + 1,
-                        'expected_results': cursor['expected_results'] + self.page_size
+                        "last_cursor": self.get_next_cursor(),
+                        "page_number": cursor["page_number"] + 1,
+                        "expected_results": cursor["expected_results"]
+                        + self.settings.page_size,
                     }
                     self.save_last_cursor(cursor)
 
@@ -82,25 +80,26 @@ class Database_ipi_ch():
 
     def send_post(self, cursor):
         headers = {
-            'content-type': 'application/transit+json',
-            'X-IPI-VERSION': '2.1.3',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
+            "content-type": "application/transit+json",
+            "X-IPI-VERSION": "2.1.3",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36",
         }
 
         self.response = self.session.post(
-            f'https://database.ipi.ch/database/resources/query/fetch?ps={self.page_size}',
+            f"https://database.ipi.ch/database/resources/query/fetch?ps={self.settings.page_size}",
             headers=headers,
-            json=post_data_from_oldest(self.page_size, cursor))
+            json=post_data_from_oldest(self.settings.page_size, cursor),
+        )
 
     def get_next_cursor(self):
-        cursor_result = self.response.json()['metadataAsTransit']
-        cursor = cursor_result.replace('\\', '')
-        return json.loads(cursor)['~#resultmeta']['~:cursor']['~#opt']
+        cursor_result = self.response.json()["metadataAsTransit"]
+        cursor = cursor_result.replace("\\", "")
+        return json.loads(cursor)["~#resultmeta"]["~:cursor"]["~#opt"]
 
     def get_total_items(self):
-        cursor_result = self.response.json()['metadataAsTransit']
-        cursor = cursor_result.replace('\\', '')
-        return json.loads(cursor)['~#resultmeta']['~:total-items']
+        cursor_result = self.response.json()["metadataAsTransit"]
+        cursor = cursor_result.replace("\\", "")
+        return json.loads(cursor)["~#resultmeta"]["~:total-items"]
 
     def set_total_items(self):
         self.total_items = self.get_total_items()
@@ -108,27 +107,25 @@ class Database_ipi_ch():
     def base64_decoded_utf8(self, image_slug):
         url = f"https://database.ipi.ch/database/resources/image/{image_slug}"
         base64_encoded_data = base64.b64encode(requests.get(url).content)
-        return base64_encoded_data.decode('utf-8')
+        return base64_encoded_data.decode("utf-8")
 
     def transform_to_image(self, base64_img):
-        base64_img_bytes = base64_img.encode('UTF-8')
-        with open('decoded_image.png', 'wb') as file_to_save:
+        base64_img_bytes = base64_img.encode("UTF-8")
+        with open("decoded_image.png", "wb") as file_to_save:
             decoded_image_data = base64.decodebytes(base64_img_bytes)
             file_to_save.write(decoded_image_data)
 
     def save_last_cursor(self, last_cursor):
-        with open(FLAGS.last_cursor_file, 'w') as f:
+        with open(self.settings.last_cursor_file, "w") as f:
             json.dump(last_cursor, f)
 
     def get_last_cursor_object(self):
         """Returns a dictionary from last_cursor file"""
-        with open(FLAGS.last_cursor_file, 'r') as json_file:
+        with open(self.settings.last_cursor_file, "r") as json_file:
             cursor = json.load(json_file)
             return cursor
 
-def main(_):
+
+if __name__ == "__main__":
     console.print("Initializing!", style="green on black")
     Database_ipi_ch().scrape()
-
-if __name__ == '__main__':
-    app.run(main)

30837ff26054233810216c9d30091f1d016ae6ed
marc <marcmaligmat@gmail.com>
2021-10-19 22:27:54 +0800

    refactor code of enqueue request method

--- scrapers/entscheidsuche/main.py
+++ scrapers/entscheidsuche/main.py
@@ -19,7 +19,7 @@ class EntscheidsucheCH(dj_scrape.core.CouchDBMixin, dj_scrape.core.Scraper):
         num_request_workers = 1
         num_results_workers = 1
         http_pause_seconds = 0.1
-        max_results_batch_size = 8
+        max_results_batch_size = 1
 
     async def initialize(self):
         await super().initialize()
@@ -27,60 +27,53 @@ class EntscheidsucheCH(dj_scrape.core.CouchDBMixin, dj_scrape.core.Scraper):
         async with self.http_request(start_url) as response:
             tree = html.fromstring(html=await response.text())
         links = tree.xpath('//a[contains(@href,"/docs")]/@href')
-        for link in links:
-            await self.enqueue_request(link)
+
+        for link in links[:2]:
+            request_url = urljoin(self.ROOT_URL, link)
+            async with self.http_request(request_url) as response:
+                response_text = await response.text()
+
+            tree = html.fromstring(html=response_text)
+            file_links = tree.xpath(
+                '//a[contains(@href,"/docs") and text() != "Parent Directory"]/@href'
+            )
+
+            # get distinct links
+            distinct_links = list(dict.fromkeys(
+                map(self.remove_extension, file_links)))
+
+            for distinct_link in distinct_links:
+                await self.enqueue_request(distinct_link)
 
     async def handle_request(self, request):
-        request_url = urljoin(self.ROOT_URL, request)
-        async with self.http_request(request_url) as response:
-            response_text = await response.text()
+        parsed = await self.parse(request)
+        if parsed is not None:
+            await self.enqueue_result(parsed)
 
-        tree = html.fromstring(html=response_text)
-        file_links = tree.xpath(
-            '//a[contains(@href,"/docs") and text() != "Parent Directory"]/@href'
-        )
-        current_link = ""
-        attachments = []
-        entry = {}
-        original_link = ""
-        for _link in file_links:
-            __link = re.sub(r"\.\w+$", "", _link)
-            folder = re.search(r"\/docs\/([^\/]+)", __link).group(1)
-            entry["folder"] = folder
-            entry["attachments"] = attachments
-
-            if current_link != __link:
-
-                if current_link != "":
-                    l = re.sub(r"\/.+\/", "", original_link)
-                    entry["id"] = re.sub(r"\.\w+$", "", l)
-
-                    parsed = await self.parse(request_url, entry)
-                    if parsed is not None:
-                        await self.enqueue_result(parsed)
-
-                current_link = __link
-                attachments = [_link]
-
-            else:
-                original_link = _link
-                attachments.append(_link)
-
-    async def parse(self, url, entry):
+    async def parse(self, url):
         try:
+            _url = urljoin(self.ROOT_URL, url)
+            folder = re.search(r"\/docs\/([^\/]+)", url).group(1)
+            _id = re.search(r"\/([^\/]+)$", url).group(1)
+
+            entry = {
+                "url": _url,
+                "folder": folder,
+                "id": _id,
+            }
             attachments = {}
 
-            for file_link in entry["attachments"]:
+            attchment_links = [
+                urljoin(self.ROOT_URL, url + '.json'),
+                urljoin(self.ROOT_URL, url + '.html'),
+                urljoin(self.ROOT_URL, url + '.pdf')
+            ]
+
+            for file_link in attchment_links:
                 fname, fcontent = await self.get_file(file_link)
                 attachments[fname] = fcontent
 
-            final_entry = {
-                "url": url,
-                "folder": entry["folder"],
-                "id": entry["id"],
-            }
-
-            return final_entry, attachments
+            return entry, attachments
         except:
             logger.exception(url)
 
@@ -103,9 +96,11 @@ class EntscheidsucheCH(dj_scrape.core.CouchDBMixin, dj_scrape.core.Scraper):
     async def get_file(self, dl_link):
         dl_link = urljoin(self.ROOT_URL, dl_link)
         file_url = dl_link.replace("\\", "")
-        filename = dl_link
         async with self.http_request(file_url) as resp:
-            return filename, await resp.read()
+            return dl_link, await resp.read()
+
+    def remove_extension(self, url):
+        return re.sub(r'\.[^\.]+$', '', url)
 
 
 def main(_):

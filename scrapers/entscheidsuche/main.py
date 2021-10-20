313f2ff833f48348569c61152a7f79c35e998b8e
marc <marcmaligmat@gmail.com>
2021-10-20 13:40:26 +0800

    fix problem on enqueue that is not a link

--- scrapers/entscheidsuche/main.py
+++ scrapers/entscheidsuche/main.py
@@ -28,7 +28,10 @@ class EntscheidsucheCH(dj_scrape.core.CouchDBMixin, dj_scrape.core.Scraper):
             tree = html.fromstring(html=await response.text())
         links = tree.xpath('//a[contains(@href,"/docs")]/@href')
 
-        for link in links[:2]:
+        for link in links:
+            if "." in link:
+                continue
+
             request_url = urljoin(self.ROOT_URL, link)
             async with self.http_request(request_url) as response:
                 response_text = await response.text()

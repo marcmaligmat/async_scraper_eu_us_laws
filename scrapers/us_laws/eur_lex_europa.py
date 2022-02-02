dfa581b19cd2a0f8af23b753a87d109694a242b3
marc <marcmaligmat@gmail.com>
2022-02-02 17:16:06 +0800

    download pdf files for EN, FR, IT and DE

--- scrapers/us_laws/eur_lex_europa.py
+++ scrapers/us_laws/eur_lex_europa.py
@@ -33,32 +33,22 @@ class EurLexEurpa(dj_scrape.core.CouchDBMixin, dj_scrape.core.Scraper):
                 # next_page = t.xpath('//img[@title="Show next document"]/../@href')[0]
                 # # table rows
                 trows = t.xpath("//tbody/tr")
-
-                for trow in trows[4:5]:
-
+                # for trow in trows[4:5]:
+                for trow in trows:
                     try:
 
                         date = trow.xpath("./td[1]/text()")[0]
+
+                        # legislation links and parsing
                         leg_links = trow.xpath("./td[2]/a/@href")
-                        for leg_link in leg_links:
-                            entry = {}
-                            attachments = {}
+                        leg_names = trow.xpath("./td[2]/a/text()")
+                        await self.parse_trows(date, leg_links, leg_names)
 
-                            f_link = urljoin(
-                                self.ROOT_URL, leg_link.replace("./../../", "")
-                            )
+                        # for information and notices
+                        info_links = trow.xpath("./td[3]/a/@href")
+                        info_names = trow.xpath("./td[3]/a/text()")
+                        await self.parse_trows(date, info_links, info_names)
 
-                            entry = {
-                                "url": f_link,
-                                "date": date,
-                            }
-                            logger.info(f"Scraping {f_link}")
-                            fname, fcontent = await self.get_html(f_link)
-                            attachments[fname] = fcontent
-
-                            parsed = await self.parse(entry, attachments)
-                            if parsed is not None:
-                                await self.enqueue_result(parsed)
                     except:
                         logger.exception(f"Error on {date}")
 
@@ -66,6 +56,32 @@ class EurLexEurpa(dj_scrape.core.CouchDBMixin, dj_scrape.core.Scraper):
                 #     await self.enqueue_request(next_page)
         except:
             logger.exception(request)
+
+    async def parse_trows(self, date, links, names):
+        for i, leg_link in enumerate(links):
+            entry = {}
+            attachments = {}
+
+            leg_name = names[i]
+
+            f_link = urljoin(self.ROOT_URL, leg_link.replace("./../../", ""))
+
+            entry = {
+                "url": f_link,
+                "leg_name": leg_name,
+                "date": date,
+            }
+            logger.info(f"Scraping {f_link}")
+
+            # languages
+            langs = ["EN", "FR", "IT", "DE"]
+            for lang in langs:
+                fname, fcontent = await self.follow_link(f_link, lang)
+                attachments[fname] = fcontent
+
+            parsed = await self.parse(entry, attachments)
+            if parsed is not None:
+                await self.enqueue_result(parsed)
 
     async def parse(self, entry, attachments):
         try:
@@ -76,18 +92,15 @@ class EurLexEurpa(dj_scrape.core.CouchDBMixin, dj_scrape.core.Scraper):
     async def handle_results(self, results):
         collection = await self.get_db(self.DB_NAME)
         for entry, attachments in results:
-            url = entry["url"]
-            async with self.get_doc(collection, url) as doc:
+            _id = entry["date"] + " " + entry["leg_name"]
+            async with self.get_doc(collection, _id) as doc:
                 doc.update(entry)
             for file_name, file_content in attachments.items():
 
                 try:
-                    if "getattachment" in file_name:
-                        await doc.attachment(file_name).save(file_content, "image/jpeg")
-                    else:
-                        await doc.attachment(file_name).save(
-                            file_content, "application/pdf"
-                        )
+                    await doc.attachment(file_name).save(
+                        file_content, "application/pdf"
+                    )
                     logger.info(f"Downloading {file_name}")
                 except:
                     logger.debug(f"Cannot download {file_name}")
@@ -100,19 +113,21 @@ class EurLexEurpa(dj_scrape.core.CouchDBMixin, dj_scrape.core.Scraper):
         else:
             return "", ""
 
-    async def get_html(self, link):
+    async def follow_link(self, link, lang):
         if link:
-            logger.info(f"Downloading PDF file of {link}")
+            logger.info(f"Downloading PDF files of {link}")
             async with self.http_request(link) as response:
                 t = html.fromstring(html=await response.text())
 
-            pdf_link = t.xpath(
-                '//li/a[contains(@id,"format_language_table_PDF_EN")]/@href'
-            )[0]
-
-            return await self.get_file(
-                urljoin(self.ROOT_URL, pdf_link.replace("./../../../", ""))
-            )
+            try:
+                pdf_link = t.xpath(
+                    f'//li/a[contains(@id,"format_language_table_PDF_{lang}")]/@href'
+                )[0]
+                return await self.get_file(
+                    urljoin(self.ROOT_URL, pdf_link.replace("./../../../", ""))
+                )
+            except:
+                return "", ""
 
 
 def main():
